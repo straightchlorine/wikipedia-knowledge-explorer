650308a03bf984752ddb50d87be562f130ee4e06
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2025-03-27 23:56:11 +0100

    fix(CORS): added the CORS middleware

--- backend/wiki/main.py
+++ backend/wiki/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from wiki.routes import articles
 
 wiki_exp = FastAPI()
 
 wiki_exp.include_router(articles.router)
+
+wiki_exp.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @wiki_exp.get("/")
