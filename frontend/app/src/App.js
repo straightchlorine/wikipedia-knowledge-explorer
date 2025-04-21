b35cb086ddbde3b4dc5a08444323c6c9c8fe38ad
Daniel Kalmucki <danielkalmucki2@gmail.com>
2025-04-21 13:05:16 +0200

    Implement Basic Graph Visualization View

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import About from "./pages/Contact";
 import NoPage from "./pages/NoPage";
+import Visualize from "./pages/Visualize";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Route index element={<Home />} />
                     <Route path="explore" element={<Explore />} />
                     <Route path="about" element={<About />} />
+                    <Route path="visualize" element={<Visualize />} />
                     <Route path="*" element={<NoPage />} />
                 </Route>
             </Routes>
--- frontend/app/src/pages/Explore.js
+++ frontend/app/src/pages/Explore.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import { Search } from "lucide-react";
 import "./Explore.css"
+import LoadingDots from "../common/LoadingDots"
+import ErrorTag from "../common/ErrorTag"
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
 
@@ -37,26 +40,22 @@ const SearchBar = () => {
     );
 };
 
-const LoadingDots = () => {
-    return (
-        <div className="loading-dots">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-    );
-};
-
 const DataComponent = ({ query }) => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleRedirect = () => {
+        navigate(`/visualize?query=${query}`);
+    };
+
 
     useEffect(() => {
         const fetchData = async () => {
             console.log(`Fetching ${API_BASE_URL}/articles/clusters?query=${query}"`);
             try {
-                const response = await fetch(`${API_BASE_URL}/articles/clusters?query=${query}"`);
+                const response = await fetch(`${API_BASE_URL}/articles/?query=${encodeURIComponent(query)}"`);
                 if (!response.ok) throw new Error("Failed to fetch data");
                 const result = await response.json();
                 setData(result);
@@ -70,15 +69,14 @@ const DataComponent = ({ query }) => {
     }, [query]);
 
     if (loading) return <LoadingDots />;
-    if (error) return <p>Error: {error}</p>;
+    if (error) return <ErrorTag error={error} />;
 
     return (
         <div>
-            <div>
-                <h2>Results</h2>
-                <div className="result-list">
-                    {data.articles.map((item, index) => <div key={index}><p>{item.title}</p></div>)}
-                </div>
+            <h2>Results</h2>
+            <h3 onClick={handleRedirect}>Visualize</h3>
+            <div className="result-list">
+                {data.articles.map((item, index) => <div key={index}><p>{item}</p></div>)}
             </div>
         </div>
     );
