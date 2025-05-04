7c877f59c1414f3759abff17f77d1304dbf3c6da
Daniel Kalmucki <danielkalmucki2@gmail.com>
2025-05-04 12:39:27 +0200

    Introduce UnitTests

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
@@ -9,17 +9,15 @@ import Visualize from "./pages/Visualize";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="explore" element={<Explore />} />
-                    <Route path="about" element={<About />} />
-                    <Route path="visualize" element={<Visualize />} />
-                    <Route path="*" element={<NoPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="explore" element={<Explore />} />
+                <Route path="about" element={<About />} />
+                <Route path="visualize" element={<Visualize />} />
+                <Route path="*" element={<NoPage />} />
+            </Route>
+        </Routes>
     );
 }
 
